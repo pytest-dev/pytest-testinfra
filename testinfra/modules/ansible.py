6e7c8ad2b8fe4d005221943d743eac2fc4eadb9c
Philippe Pepiot <phil@philpep.org>
2016-05-15 01:50:07 +0200

    normalize the license header and coding directive
    Don't want to maintain the copyright year & author

--- testinfra/modules/ansible.py
+++ testinfra/modules/ansible.py
@@ -1,6 +1,4 @@
-# -*- coding: utf8 -*-
-# Copyright © 2015-2016 Philippe Pepiot <phil@philpep.org>
-#
+# coding: utf-8
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
