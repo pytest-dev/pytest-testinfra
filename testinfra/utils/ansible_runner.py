50c275ca186aa0e09bc377afc469f8bb931669e6
Philippe Pepiot <phil@philpep.org>
2017-09-21 00:11:10 +0200

    Run tests with python 3.5
    Because ansible require 3.5 and 3.5 is the default on debian stretch.
    Also since pylint now run with py35, disable some false positive in
    ansible compat module.

--- testinfra/utils/ansible_runner.py
+++ testinfra/utils/ansible_runner.py
@@ -11,7 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# pylint: disable=import-error
+# pylint: disable=import-error,no-name-in-module,no-member
 
 from __future__ import unicode_literals
 from __future__ import absolute_import
