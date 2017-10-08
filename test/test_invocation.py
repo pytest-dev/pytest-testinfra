0e973a77c620a0ed191816a341abf53a996407a5
Philippe Pepiot <phil@philpep.org>
2017-10-08 19:03:09 +0200

    Fix --nagios with py3
    Nagios output was broken on py3, mostly related to str vs bytes.
    Add invocation tests using pytester plugin.

---
+++ test/test_invocation.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import unicode_literals
+
+pytest_plugins = ['pytester']
+
+
+def test_nagios_notest(testdir):
+    result = testdir.runpytest('--nagios', '-q', '--tb=no')
+    assert result.ret == 0
+    stdout = result.stdout.str()
+    assert stdout.startswith('TESTINFRA OK - 0 passed, 0 failed, 0 skipped')
+
+
+def test_nagios_ok(testdir):
+    testdir.makepyfile('def test_ok(): pass')
+    result = testdir.runpytest('--nagios', '-q', '--tb=no')
+    assert result.ret == 0
+    stdout = result.stdout.str()
+    assert stdout.startswith('TESTINFRA OK - 1 passed, 0 failed, 0 skipped')
+    assert '.' in stdout.splitlines()
+
+
+def test_nagios_fail(testdir):
+    testdir.makepyfile('def test_ok(): pass\ndef test_fail(): assert False')
+    result = testdir.runpytest('--nagios', '-q', '--tb=no')
+    assert result.ret == 2
+    stdout = result.stdout.str()
+    assert stdout.startswith(
+        'TESTINFRA CRITICAL - 1 passed, 1 failed, 0 skipped')
+    assert '.F' in stdout.splitlines()
