77869f534465caf93dd90205bb119c0ff688d0d4
Philippe Pepiot <phil@philpep.org>
2017-10-22 16:51:02 +0200

    Use cached_property for paramiko client

--- testinfra/backend/paramiko.py
+++ testinfra/backend/paramiko.py
@@ -26,6 +26,7 @@ except ImportError:
 import paramiko.ssh_exception
 
 from testinfra.backend import base
+from testinfra.utils import cached_property
 
 
 class IgnorePolicy(paramiko.MissingHostKeyPolicy):
@@ -43,40 +44,37 @@ class ParamikoBackend(base.BaseBackend):
         self.host = self.parse_hostspec(hostspec)
         self.ssh_config = ssh_config
         self.ssh_identity_file = ssh_identity_file
-        self._client = None
         super(ParamikoBackend, self).__init__(self.host.name, *args, **kwargs)
 
-    @property
+    @cached_property
     def client(self):
-        if self._client is None:
-            client = paramiko.SSHClient()
-            client.set_missing_host_key_policy(paramiko.WarningPolicy())
-            cfg = {
-                "hostname": self.host.name,
-                "port": int(self.host.port) if self.host.port else 22,
-                "username": self.host.user,
-            }
-            if self.ssh_config:
-                ssh_config = paramiko.SSHConfig()
-                with open(self.ssh_config) as f:
-                    ssh_config.parse(f)
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.WarningPolicy())
+        cfg = {
+            "hostname": self.host.name,
+            "port": int(self.host.port) if self.host.port else 22,
+            "username": self.host.user,
+        }
+        if self.ssh_config:
+            ssh_config = paramiko.SSHConfig()
+            with open(self.ssh_config) as f:
+                ssh_config.parse(f)
 
-                for key, value in ssh_config.lookup(self.host.name).items():
-                    if key == "hostname":
-                        cfg[key] = value
-                    elif key == "user":
-                        cfg["username"] = value
-                    elif key == "port":
-                        cfg[key] = int(value)
-                    elif key == "identityfile":
-                        cfg["key_filename"] = os.path.expanduser(value[0])
-                    elif key == "stricthostkeychecking" and value == "no":
-                        client.set_missing_host_key_policy(IgnorePolicy())
-            if self.ssh_identity_file:
-                cfg["key_filename"] = self.ssh_identity_file
-            client.connect(**cfg)
-            self._client = client
-        return self._client
+            for key, value in ssh_config.lookup(self.host.name).items():
+                if key == "hostname":
+                    cfg[key] = value
+                elif key == "user":
+                    cfg["username"] = value
+                elif key == "port":
+                    cfg[key] = int(value)
+                elif key == "identityfile":
+                    cfg["key_filename"] = os.path.expanduser(value[0])
+                elif key == "stricthostkeychecking" and value == "no":
+                    client.set_missing_host_key_policy(IgnorePolicy())
+        if self.ssh_identity_file:
+            cfg["key_filename"] = self.ssh_identity_file
+        client.connect(**cfg)
+        return client
 
     def _exec_command(self, command):
         chan = self.client.get_transport().open_session()
@@ -94,7 +92,7 @@ class ParamikoBackend(base.BaseBackend):
         except paramiko.ssh_exception.SSHException:
             if not self.client.get_transport().is_active():
                 # try to reinit connection (once)
-                self._client = None
+                del self.client
                 rc, stdout, stderr = self._exec_command(command)
             else:
                 raise
