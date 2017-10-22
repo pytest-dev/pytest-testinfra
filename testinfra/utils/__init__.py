92954ac8295e447b9c38dd87f242a16a6839e543
Philippe Pepiot <phil@philpep.org>
2017-10-22 16:49:21 +0200

    Introduce a cached_property decorator
    From https://pypi.python.org/pypi/cached-property

--- testinfra/utils/__init__.py
+++ testinfra/utils/__init__.py
@@ -22,3 +22,22 @@ all_cap_re = re.compile('([a-z0-9])([A-Z])')
 def un_camel_case(name):
     s1 = first_cap_re.sub(r'\1_\2', name)
     return all_cap_re.sub(r'\1_\2', s1).lower()
+
+
+class cached_property(object):
+    """A cached property computed only once per instance
+
+    A property that is only computed once per instance and then replaces itself
+    with an ordinary attribute. Deleting the attribute resets the property.
+    Source: https://github.com/bottlepy/bottle/commit/fa7733e075da0d790d809aa3d2f53071897e6f76
+    """  # noqa
+
+    def __init__(self, func):
+        self.__doc__ = getattr(func, '__doc__')
+        self.func = func
+
+    def __get__(self, obj, cls):
+        if obj is None:
+            return self
+        value = obj.__dict__[self.func.__name__] = self.func(obj)
+        return value
