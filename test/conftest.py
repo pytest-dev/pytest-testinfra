489ece1db87d6c8f71c2c7da2558c7797d9209c2
Kirill <kirill_chuprin@localhost.localdomain>
2017-12-18 13:59:29 +0700

    fix

--- test/conftest.py
+++ test/conftest.py
@@ -113,7 +113,8 @@ def build_docker_container_fixture(image, scope):
             docker_host = "localhost"
 
         cmd = ["docker", "run", "-d", "-P"]
-        if image in ("ubuntu_xenial", "debian_jessie", "centos_7", "fedora", "alpine_35"):
+        if image in ("ubuntu_xenial", "debian_jessie", "centos_7", "fedora",
+                     "alpine_35"):
             cmd.append("--privileged")
 
         cmd.append("philpep/testinfra:" + image)
