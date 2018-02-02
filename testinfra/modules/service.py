499cf392ed2502f7212b4b4c61c94c43d43ed7c8
Philippe Pepiot <phil@philpep.org>
2018-02-02 12:00:05 +0100

    pylint disable too many return statements

--- testinfra/modules/service.py
+++ testinfra/modules/service.py
@@ -45,6 +45,7 @@ class Service(Module):
         """Test if service is enabled"""
         raise NotImplementedError
 
+    # pylint: disable=too-many-return-statements
     @classmethod
     def get_module_class(cls, host):
         if host.system_info.type == "linux":
