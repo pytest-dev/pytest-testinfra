860d96bdaaaf82a88e0b28d50e3ebbf7cc2bf179
Catalin Codreanu <spoiledkefir@gmail.com>
2018-12-12 14:32:49 +0100

    working on connectivity tests

---
+++ test/basic_test.py
@@ -0,0 +1,6 @@
+def test_passwd_file(host):
+    passwd = host.file("/etc/passwd")
+    assert passwd.contains("root")
+    assert passwd.user == "root"
+    assert passwd.group == "root"
+    assert passwd.mode == 0o644
