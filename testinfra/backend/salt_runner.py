98d3b79dda8452e7c530209d5baf7d1404ab405a
Jean-Yves NOLEN <jean-yves.nolen@ubisoft.com>
2019-01-21 18:41:31 +0100

    Missing files

---
+++ testinfra/backend/salt_runner.py
@@ -0,0 +1,62 @@
+# coding: utf-8
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import unicode_literals
+from __future__ import absolute_import
+
+try:
+    import salt.runner
+    import salt.config
+except ImportError:
+    raise RuntimeError(
+        "You must install salt package to use the salt runner backend")
+
+from testinfra.backend import base
+
+
+class SaltRunnerBackend(base.BaseBackend):
+    HAS_RUN_SALT = True
+    NAME = "salt_run"
+
+    def __init__(self, host, *args, **kwargs):
+        self.host = host
+        self.config = "/etc/salt/master"
+        self._runner = None
+        super(SaltRunnerBackend, self).__init__(self.host, *args, **kwargs)
+
+    @property
+    def runner(self):
+        if self._runner is None:
+            self._runner = salt.runner.RunnerClient(salt.config.client_config(self.config))
+        return self._client
+
+    def run(self, command, *args, **kwargs):
+        command = self.get_command(command, *args)
+        out = self.run_salt(command, config=None, args=args, kwargs=kwargs)
+        return self.result(out['retcode'], command, out['stdout'],
+                           out['stderr'])
+
+    def run_salt(self, func, config=None, args=None, **kwargs):
+        if config is not None:
+            self.config = config
+        out = {}
+        try:
+            out["stdout"] = self.runner.cmd(func, arg=args or [], kwargs=kwargs)
+            out["retcode"] = 0
+        except KeyError:
+            out["stderr"] = "Unknown function, check salt-run -d for complete list"
+            out['retcode'] = 1
+        except ValueError:
+            out["stderr"] = "No result found"
+            out['retcode'] = 2
+        return out
---
+++ testinfra/modules/salt_run.py
@@ -0,0 +1,55 @@
+# coding: utf-8
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import unicode_literals
+
+
+import json
+
+import six
+
+from testinfra.modules.base import InstanceModule
+
+
+class SaltRun(InstanceModule):
+    """Run salt module functions
+
+
+    >>> host.salt_run("manage.up", "")
+    '1.6.2-5'
+    >>> host.salt_run("pkg.version", ["nginx", "php5-fpm"])
+    {'nginx': '1.6.2-5', 'php5-fpm': '5.6.7+dfsg-1'}
+    >>> host.salt_run("grains.item", ["osarch", "mem_total", "num_cpus"])
+    {'osarch': 'amd64', 'num_cpus': 4, 'mem_total': 15520}
+
+    Run ``salt-run sys.doc`` to get a complete list of functions
+    """
+
+    def __call__(self, function, args=None, config=None, **kwargs):
+        args = args or []
+        if isinstance(args, six.string_types):
+            args = [args]
+        if self._host.backend.HAS_RUN_SALT:
+            return self._host.backend.run_salt(function, args, config, kwargs)["stdout"]
+        else:
+            cmd_args = []
+            cmd = "salt-run --out=json"
+            if config is not None:
+                cmd += " -c %s"
+                cmd_args.append(config)
+            cmd += " %s" + len(args) * " %s"
+            cmd_args += [function] + args
+            return json.loads(self.check_output(cmd, *cmd_args))
+
+    def __repr__(self):
+        return "<salt_run>"
