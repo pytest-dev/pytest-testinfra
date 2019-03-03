47757200287e6b9997965b8905eef0497367dfe6
Julien <jul.collas@gmail.com>
2019-03-03 22:16:51 +0100

    flake8

--- test/test_modules.py
+++ test/test_modules.py
@@ -484,8 +484,9 @@ def test_pip_package(host):
 
 
 def test_x509(host):
-    host.check_output('''openssl req -new -newkey rsa:4096 -days 365 -nodes
-        -x509 -subj "/CN=test.com" -keyout /tmp/test.key -out /tmp/test.crt'''
+    host.check_output(
+        '''openssl req -new -newkey rsa:4096 -days 365 -nodes
+            -x509 -subj "/CN=test.com" -keyout /tmp/test.key -out /tmp/test.crt'''
     )
     assert host.file('/tmp/test.key').is_file
     assert host.file('/tmp/test.ctr').is_file
