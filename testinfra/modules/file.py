1a9e59e27245e5c40f596b72176bad73b8d58b7e
Matt Ralph <mralph@equalexperts.com>
2019-08-22 10:31:36 +0100

    BC-61: Add windows support for File resources

--- testinfra/modules/file.py
+++ testinfra/modules/file.py
@@ -189,6 +189,8 @@ class File(Module):
             return BSDFile
         if host.system_info.type == "darwin":
             return DarwinFile
+        if host.system_info.type == "windows":
+            return WindowsFile
         raise NotImplementedError
 
 
@@ -303,3 +305,159 @@ class NetBSDFile(BSDFile):
     def sha256sum(self):
         return self.check_output(
             "cksum -a sha256 < %s", self.path)
+
+
+class WindowsFile(File):
+    @property
+    def exists(self):
+        """Test if file exists
+        >>> host.file(r"C:/Users").exists
+        True
+        >>> host.file(r"C:/nonexistent").exists
+        False
+        """
+        return self.check_output("powershell -command \"Test-Path %s\"", self.path) == "True"
+
+    @property
+    def is_file(self):
+        return self.check_output("powershell -command \"(Get-Item %s) -is [System.IO.FileInfo]\"", self.path) == "True"
+
+    @property
+    def is_directory(self):
+        return self.check_output("powershell -command \"(Get-Item %s) -is [System.IO.DirectoryInfo]\"", self.path) == "True"
+
+    @property
+    def is_pipe(self):
+        raise NotImplementedError
+
+    @property
+    def is_socket(self):
+        raise NotImplementedError
+
+    @property
+    def is_symlink(self):
+        raise NotImplementedError
+
+    @property
+    def linked_to(self):
+        """Resolve symlink
+
+        >>> host.file("/var/lock").linked_to
+        '/run/lock'
+        """
+        raise NotImplementedError
+
+    @property
+    def user(self):
+        """Return file owner as string
+
+        >>> host.file("/etc/passwd").user
+        'root'
+        """
+        raise NotImplementedError
+
+    @property
+    def uid(self):
+        """Return file user id as integer
+
+        >>> host.file("/etc/passwd").uid
+        0
+        """
+        raise NotImplementedError
+
+    @property
+    def group(self):
+        raise NotImplementedError
+
+    @property
+    def gid(self):
+        raise NotImplementedError
+
+    @property
+    def mode(self):
+        """Return file mode as octal integer
+
+        >>> host.file("/etc/passwd").mode
+        384  # 0o600 (octal)
+        >>> host.file("/etc/password").mode == 0o600
+        True
+        >>> oct(host.file("/etc/password").mode) == '0600'
+        True
+
+        Note: Python 3 oct(x)_ function will produce ``'0o600'``
+
+        You can also utilize the file mode constants from
+        the stat_ library for testing file mode.
+
+        >>> import stat
+        >>> host.file("/etc/password").mode == stat.S_IRUSR | stat.S_IWUSR
+        True
+
+        .. _oct(x): https://docs.python.org/3.5/library/functions.html#oct
+        .. _stat: https://docs.python.org/2/library/stat.html
+        """
+        raise NotImplementedError
+
+    def contains(self, pattern):
+        return self.run_test("grep -qs -- %s %s", pattern, self.path).rc == 0
+
+    @property
+    def md5sum(self):
+        raise NotImplementedError
+
+    @property
+    def sha256sum(self):
+        raise NotImplementedError
+
+    def _get_content(self, decode):
+        out = self.run_test("cat -- %s", self.path)
+        if out.rc != 0:
+            raise RuntimeError("Unexpected output %s" % (out,))
+        if decode:
+            return out.stdout
+        return out.stdout_bytes
+
+    @property
+    def content(self):
+        """Return file content as bytes
+
+        >>> host.file("/tmp/foo").content
+        b'caf\\xc3\\xa9'
+        """
+        return self._get_content(False)
+
+    @property
+    def content_string(self):
+        """Return file content as string
+
+        >>> host.file("/tmp/foo").content_string
+        'café'
+        """
+        return self._get_content(True)
+
+    @property
+    def mtime(self):
+        """Return time of last modification as datetime.datetime object
+
+        >>> host.file("/etc/passwd").mtime
+        datetime.datetime(2015, 3, 15, 20, 25, 40)
+        """
+        raise NotImplementedError
+
+    @property
+    def size(self):
+        """Return size of file in bytes"""
+        raise NotImplementedError
+
+    def __repr__(self):
+        return "<file %s>" % (self.path,)
+
+    def __eq__(self, other):
+        if isinstance(other, File):
+            return self.path == other.path
+        if isinstance(other, six.string_types):
+            return self.path == other
+        return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
