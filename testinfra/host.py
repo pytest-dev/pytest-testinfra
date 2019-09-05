c9320bce0708fce074ecb3e1cb06591b99f4c0ee
Ian Wienand <iwienand@redhat.com>
2019-09-06 09:39:44 +1000

    [dnm] testing opendev ci
    Depends-On: https://review.opendev.org/680895

--- testinfra/host.py
+++ testinfra/host.py
@@ -11,6 +11,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#
+# testing only
+#
+
 from __future__ import unicode_literals
 
 import os
