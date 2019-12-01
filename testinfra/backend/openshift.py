6ddd90d1fab75641d85d83664838ec20d17eaf9f
Philippe Pepiot <phil@philpep.org>
2019-12-01 18:33:21 +0100

    Drop "shell" parameter from openshift backend
    As mentioned in https://github.com/philpep/testinfra/pull/516 I'm not
    sure we should implement this as it.

--- testinfra/backend/openshift.py
+++ testinfra/backend/openshift.py
@@ -25,7 +25,6 @@ class OpenShiftBackend(base.BaseBackend):
         self.container = kwargs.get('container')
         self.namespace = kwargs.get('namespace')
         self.kubeconfig = kwargs.get('kubeconfig')
-        self.shell = kwargs.get('shell', '/bin/sh -c')
         super(OpenShiftBackend, self).__init__(self.name, *args, **kwargs)
 
     def run(self, command, *args, **kwargs):
@@ -43,7 +42,7 @@ class OpenShiftBackend(base.BaseBackend):
         if self.container is not None:
             oscmd += '-c %s '
             oscmd_args.append(self.container)
-        oscmd += 'exec %s -- {shell} %s'.format(shell=self.shell)
+        oscmd += 'exec %s -- /bin/sh -c %s'
         oscmd_args.extend([self.name, cmd])
         out = self.run_local(oscmd, *oscmd_args)
         return out
