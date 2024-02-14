fb02a9bce47601d23f8c923494c5e12dc5251859
Martin Hoyer <mhoyer@redhat.om>
2024-02-14 10:28:41 +0100

    Use future annotations for pytest<7 compatibility

--- testinfra/plugin.py
+++ testinfra/plugin.py
@@ -10,6 +10,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import annotations
+
 import logging
 import shutil
 import sys
